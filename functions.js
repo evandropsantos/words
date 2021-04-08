6cdc51283b49386a13783e31a1381d7ba2522c1f
Evandro P. Santos <evandropsantos@gmail.com>
2021-04-08 16:27:26 -0300

    feat(funcoes): funcao para ler arquivos

--- functions.js
+++ functions.js
@@ -1,6 +1,20 @@
 const fs = require('fs')
 const path = require('path')
 
+function readFile (address) {
+  return new Promise((resolve, reject) => {
+    try {
+      resolve(
+        fs.readFileSync(address, { encoding: 'utf-8' }).toString()
+      )
+    } catch (error) { reject(error) }
+  })
+}
+
+function readFiles (addresses) {
+  return Promise.all(addresses.map((address) => readFile(address)))
+}
+
 function readFolder (address) {
   return new Promise((resolve, reject) => {
     try {
@@ -12,5 +26,6 @@ function readFolder (address) {
 }
 
 module.exports = {
+  readFiles,
   readFolder
 }
